d80a44bb8053e5d78fa55791fdd8aa252bee83a5
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-10 16:26:17 +0530

    Update __init__.py

--- Wylie/modules/__init__.py
+++ Wylie/modules/__init__.py
@@ -26,3 +26,28 @@ def get_readable_time(seconds: int) -> str:
     ping_time += ":".join(time_list)
 
     return ping_time
+
+
+async def get_user(event):
+    args = event.pattern_match.group(1).split(" ", 1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await tbot.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        extra = None
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.reply("I don't know who you're talking about, you're going to need to specify a user...!")
+            return
+        try:
+            user_obj = await tbot.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.reply(str(err))
+            return
+
+    return user_obj, extra
