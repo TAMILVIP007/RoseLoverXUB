974e2f7fa72e44dcf6652127c4c94e064845b44a
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-10 21:14:35 +0530

    Create Admin.py

---
+++ Wylie/modules/Admin.py
@@ -0,0 +1,22 @@
+from Wylie import ubot, tbot
+from Wylie.events import Wbot
+from . import get_user
+
+@Wbot(pattern="^/kick ?(.*)")
+async def kick(event):
+ if event.is_private:
+   return await event.edit("Please use this in groups.")
+ try:
+  user, extra = await get_user(event)
+ except TypeError:
+  pass
+ if not user:
+  await event.edit("Failed to fetch user.")
+ perm = await ubot.get_permissions(event.chat_id, event.sender_id)
+ if not perm.is_admin or not perm.ban_users:
+   return await event.edit("Failed to Kick, ChatAdminRequired.")
+ try:
+  await ubot.kick_participant(event.chat_id, user.id)
+  await event.edit(f"Kicked **{user.first_name}** from **{event.chat.title}**.")
+ except:
+  await event.edit("Failed to Kick.")
