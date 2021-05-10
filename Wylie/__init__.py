d08805f1b832d1200d512807f869e19d4dda543f
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-10 12:39:58 +0530

    Create __init__.py

---
+++ Wylie/__init__.py
@@ -0,0 +1,22 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from datetime import datetime
+import os, logging
+from logging import basicConfig, getLogger, INFO
+"""RoseLoverX"""
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
+
+StartTime = datetime.now()
+
+STRING = os.environ.get("STRING_SESSION")
+TOKEN = os.environ.get("TOKEN")
+OWNER_ID = os.environ.get("OWNER_ID")
+API_KEY = os.environ.get("API-KEY")
+API_HASH = os.environ.get("API_HASH")
+
+ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+tbot = TelegramClient(None, API_KEY, API_HASH)
+
