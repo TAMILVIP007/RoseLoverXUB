e14997ab1666711295ff7ba6a6bf88b80562a7b1
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-10 13:55:44 +0530

    Update alive.py

--- Wylie/modules/alive.py
+++ Wylie/modules/alive.py
@@ -1,5 +1,18 @@
 from Wylie.events import Wbot
+import time
+from datetime import datetime
+from . import get_readable_time
 
 @Wbot(pattern="^/ping")
 async def _(event):
-  await event.edit("Pong.")
+    start_time = datetime.datetime.now()
+    message = await event.edit("Pinging.")
+    end_time = datetime.datetime.now()
+    pingtime = end_time - start_time
+    telegram_ping = str(round(pingtime.total_seconds(), 2)) + "s"
+    uptime = get_readable_time((time.time() - StartTime))
+    await message.edit(
+        "<b>Pong !! </b> <code>{}</code>\n"
+        "<b>Uptime -</b> <code>{}</code>".format(telegram_ping, uptime),
+        parse_mode="html",
+    )
