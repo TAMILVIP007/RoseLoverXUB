79822864652eb13f3e8af42da6690a309526d41d
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-01 11:40:43 +0530

    Update __init__.py

--- Wylie/__init__.py
+++ Wylie/__init__.py
@@ -12,6 +12,7 @@ LOGGER = logging.getLogger(__name__)
 StartTime = time.time()
 
 STRING_SESSION = os.environ.get("STRING_SESSION")
+STACY_SESSION = os.environ.get("STACY_SESSION")
 TOKEN = os.environ.get("TOKEN")
 OWNER_ID = os.environ.get("OWNER_ID")
 API_KEY = os.environ.get("API_KEY")
@@ -19,4 +20,5 @@ API_HASH = os.environ.get("API_HASH")
 
 ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
 tbot = TelegramClient(None, API_KEY, API_HASH)
+stacy = TelegramClient(StringSession(STACY_SESSION), API_KEY, API_HASH)
 
